af9996ec24b0856480adc516775c39a965910d7c
hetanhong <543039822@qq.com>
2017-07-01 10:27:42 +0800

    modify input

--- src/containers/Login.vue
+++ src/containers/Login.vue
@@ -1,10 +1,24 @@
 <template>
     <div class="login">
-      login
+        <div class="form-warp">
+            <div class="title">登录</div>
+            <div class="form">
+                <div class="form-item" flex>
+                    <label flex-box="0" for="email">
+                        <img src="../images/login/password.png"/>
+                        邮箱
+                    </label>
+                    <input id="email" class="input" flex-box="1"/>
+                </div>
+
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+    import '../less/login.less';
     export default {
         name: 'login',
         data(){
