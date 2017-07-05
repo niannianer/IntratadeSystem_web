54dcc6145713b7e69fa977595c4383a8b0c80ce7
delehei <wangchunting7@163.com>
2017-07-05 11:00:44 +0800

    trim

--- src/components/Quota/Quota.vue
+++ src/components/Quota/Quota.vue
@@ -2,14 +2,6 @@
     <div class="quota">
         <div class="shade"></div>
         <div class="box">
-            <!-- <div class="content" flex="dir:top">
-                <p class="text" flex-box="1">是否在宝付完成充值</p>
-                <div class="btns" flex-box="0" flex="main:justify">
-                    <button class="ok"  @click.stop="btnAction(0)">已完成</button>
-                    <button  @click.stop="btnAction(1)">未完成</button>
-                </div>
-                <div class="dclose" @click.stop="btnAction(2)">×</div>
-            </div> -->
             <div class="table">
                 <h6>企业网银充值限额表</h6>
                 <ul flex class="thead">
