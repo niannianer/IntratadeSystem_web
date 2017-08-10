a4d20b3b1b40b770ebdebeb6cb8242586b731c65
hetanhong <543039822@qq.com>
2017-08-10 14:33:09 +0800

    issuer test origin

--- src/tools/config.js
+++ src/tools/config.js
@@ -3,7 +3,7 @@
  */
 let protocol = window.location.protocol;
 export const devUrl = 'http://10.10.10.69:6640';
-export const testUrl = `${protocol}//47.93.8.25:6640`;
+export const testUrl = `${protocol}//issuer-pc-test.zj-hf.cn`;
 export const productionUrl = `${protocol}//issuer-pc-prod.zj-hf.cn`;
 export const baofooCallUrlProduct = `${protocol}//callme.zj-hf.cn`;
 export const baofooCallUrlTest = testUrl;
